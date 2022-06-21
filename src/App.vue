0480e117a4b7952083de7a0315529f492cde8bc2
Flynn <ppstream123@126.com>
2022-06-21 18:49:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <h1>
+          <img src="@/assets/heart.svg" width="50" height="50" alt="">
+          Hyrule Jobs
+        </h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+    <JobListVue :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
+import JobListVue from "@/components/JobList.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    JobListVue,
+  },
+  setup() {
+    const name = ref("Hello World");
+    const age = ref<number | string>(25);
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { name, age, jobs, handleClick, order };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+      return age;
+    },
+  },
+});
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
